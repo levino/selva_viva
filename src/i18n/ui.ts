0a591a86e904fffaf8713719c335e752de543e56
Alejo <lawebdealejo@gmail.com>
2023-06-21 15:26:22 -0500

    adds nav ui names

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,26 +1,40 @@
 export const languages = {
-    de: 'De',
-    es: 'Es',
-    en: 'En'
+  de: "De",
+  es: "Es",
+  en: "En",
 };
 
+export const defaultLang = "de";
 
-  export const defaultLang = 'de';
-  
-  export const ui = {
-    
-    de: {
-      'nav.home': 'Willkomen',
-      'nav.projects': 'Proyecte',
-      'nav.about': 'Uns',
-    },
-    es: {
-        'nav.home': 'inicio',
-        'nav.projects': 'Proyectos',
-        'nav.about': 'nosotros',
-      },
-    en: {
-        'nav.home': 'home',
-        'nav.projects': 'Projects',
-    }
-  } as const;
+export const ui = {
+  de: {
+    "nav.home": "Willkomen",
+    "nav.projects": "Projekte",
+    "nav.projects.tree-nursery": "Baumschule",
+    "nav.projects.plant-a-tree": "Pflanz' einen Baum!",
+    "nav.projects.mucuna": "Mucuna",
+    "nav.projects.bamboo": "Bambus",
+    "nav.trees": "Bäume",
+    "nav.blog": "Blog",
+    "nav.contact": "Kontakt",
+    "nav.support": "Spende",
+    "nav.about": "Über uns",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.projects": "Proyectos",
+    "nav.projects.tree-nursery": "Vivero",
+    "nav.projects.plant-a-tree": "Planta un árbol",
+    "nav.projects.mucuna": "Mucuna",
+    "nav.projects.bamboo": "Guadua",
+    "nav.trees": "Árboles",
+    "nav.blog": "Blog",
+    "nav.contact": "Contacto",
+    "nav.support": "Participa",
+    "nav.about": "Uns",
+  },
+  en: {
+    "nav.home": "home",
+    "nav.projects": "Projects",
+  },
+} as const;
