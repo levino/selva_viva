4ab21782cfce012904e84b06e722564c721fbbfb
Alejo <lawebdealejo@gmail.com>
2023-06-23 18:11:54 -0500

    extends schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,7 +16,24 @@ const projectsCollection = defineCollection({
 const treesCollection = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
+    name: z.string(),
+    other_names: z.array(z.string()),
+    scientific_name: z.string(),
+    synonyms: z.array(z.string()),
+    general_information: z.string().optional(),
+    rage: z.string().optional(),
+    habitat: z.string().optional(),
+    properties: z.object({
+      conservation_status: z.string(),
+      medicinal_rating: z.number(),
+      other_uses: z.number(),
+      wood: z.number(),
+      habit: z.string(),
+      height: z.string(),
+      growth_rate: z.string(),
+      pollinators: z.string(),
+      cultivation_status: z.array(z.string()),
+    }),
   }),
 });
 
