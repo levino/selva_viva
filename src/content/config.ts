ef0ebc5ba0400029635da1bfc9955d7b4ca27afc
Alejo <lawebdealejo@gmail.com>
2023-06-23 18:20:54 -0500

    extends schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,26 +15,31 @@ const projectsCollection = defineCollection({
 
 const treesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    name: z.string(),
-    other_names: z.array(z.string()),
-    scientific_name: z.string(),
-    synonyms: z.array(z.string()),
-    general_information: z.string().optional(),
-    rage: z.string().optional(),
-    habitat: z.string().optional(),
-    properties: z.object({
-      conservation_status: z.string(),
-      medicinal_rating: z.number(),
-      other_uses: z.number(),
-      wood: z.number(),
-      habit: z.string(),
-      height: z.string(),
-      growth_rate: z.string(),
-      pollinators: z.string(),
-      cultivation_status: z.array(z.string()),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      other_names: z.array(z.string()),
+      imgs: z.array(image()),
+      scientific_name: z.string(),
+      scientific_synonyms: z.array(z.string()),
+      general_information: z.string(),
+      rage: z.string(),
+      habitat: z.string(),
+      properties: z.object({
+        conservation_status: z.string(),
+        medicinal_rating: z.number(),
+        other_uses: z.number(),
+        wood: z.number(),
+        habit: z.string(),
+        height: z.string(),
+        growth_rate: z.string(),
+        pollinators: z.string(),
+        cultivation_status: z.array(z.string()),
+      }),
+      uses: z.string(),
+      agroforestry: z.string(),
+      wood: z.string(),
     }),
-  }),
 });
 
 export const collections = {
