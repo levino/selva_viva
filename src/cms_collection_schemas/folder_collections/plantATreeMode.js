9b50927938d551ad38663683254a0efd59a58f97
Alejo <lawebdealejo@gmail.com>
2023-07-26 20:24:01 -0500

    plant a tree schema ready

---
+++ src/cms_collection_schemas/folder_collections/plantATreeMode.js
@@ -0,0 +1,82 @@
+export const plantAtreeModesSchema = {
+  name: "plant_a_tree_modalities",
+  label: "Plant a tree modalities",
+  label_singular: "Modality",
+  folder: "src/content/plantATree/",
+  identifier_field: "name",
+  create: true,
+  delete: true,
+  fields: [
+    { name: "title", widget: "string", label: "Title" },
+    {
+      name: "page_title",
+      widget: "string",
+      label: "Page Title",
+    },
+    {
+      name: "bg_image",
+      widget: "image",
+      label: "Bg Image",
+      media_folder: "/src/assets/plant_a_tree/{{field.name}}",
+    },
+    {
+      name: "bg_alt",
+      widget: "string",
+      label: "Bg Alt",
+    },
+    {
+      name: "subtitle",
+      widget: "string",
+      label: "Subtitle",
+    },
+    {
+      name: "excerpt",
+      widget: "text",
+      label: "Excerpt",
+    },
+    { name: "description_title", widget: "string", label: "Description Title" },
+    {
+      name: "description",
+      widget: "markdown",
+      label: "Description",
+    },
+    {
+      name: "what_you_get",
+      label: "What you get",
+      widget: "object",
+      fields: [
+        { name: "title", label: "Title", widget: "string" },
+        {
+          label: "List Items",
+          name: "list_items",
+          widget: "list",
+          summary: "{{fields.name}}",
+          field: { label: "Item", name: "item", widget: "string" },
+        },
+        {
+          name: "image",
+          label: "Image",
+          widget: "image",
+          media_folder: "/src/assets/plant_a_tree/{{field.name}}",
+        },
+      ],
+    },
+    {
+      name: "button_text",
+      widget: "string",
+      label: "Button Text",
+    },
+    {
+      name: "back_button_text",
+      widget: "string",
+      label: "Back Button Text",
+    },
+    {
+      name: "cover_image",
+      label: "Cover Image",
+      widget: "image",
+      media_folder: "/src/assets/plant_a_tree/{{field.name}}",
+    },
+    { name: "cover_alt", label: "Cover Alt", widget: "string" },
+  ],
+};
