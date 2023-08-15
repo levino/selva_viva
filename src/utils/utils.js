5e00a1e2fc7d0bdfb2db1bbe897a9da27550c6ee
Alejo <lawebdealejo@gmail.com>
2023-08-14 20:55:25 -0500

    little things

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 export function pickLangPath(lang) {
   return lang === "de" ? `/` : `/${lang}/`;
 }
+
+// const saveThisLink = `${pickLangPath(lang)}projects/${
+//   project.slug === "tree-nursery"
+//     ? project.slug
+//     : "plant-a-tree/" + project.slug
+// }`;
