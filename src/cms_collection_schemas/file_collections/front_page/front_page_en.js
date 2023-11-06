33694a0577a1f906427cb8116481776e0e5cce05
Alejo <lawebdealejo@gmail.com>
2023-11-05 19:59:13 -0500

    front page schema en

---
+++ src/cms_collection_schemas/file_collections/front_page/front_page_en.js
@@ -0,0 +1,8 @@
+import { frontPageSchemaFields } from "./schema";
+
+export const frontPageSchemaEn = {
+  label: "Front Page En",
+  name: "front_page_en",
+  file: "src/content/frontPage/en/index.md",
+  fields: frontPageSchemaFields,
+};
