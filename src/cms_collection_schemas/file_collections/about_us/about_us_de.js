1c06f13f4789796197fae543c829a7626056eb41
Alejo <lawebdealejo@gmail.com>
2023-11-07 09:00:12 -0500

    adds about us in german

---
+++ src/cms_collection_schemas/file_collections/about_us/about_us_de.js
@@ -0,0 +1,8 @@
+import { aboutUsPageSchemaFields } from "./about_us_schema";
+
+export const aboutUsSchemaDe = {
+  label: "About Us De",
+  name: "about_us_de",
+  file: "src/content/aboutUsPage/de/index.md",
+  fields: aboutUsPageSchemaFields,
+};
