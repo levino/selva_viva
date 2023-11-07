c62cac67f7b248b94d927b46dcc235d4a0756aaa
Alejo <lawebdealejo@gmail.com>
2023-11-07 10:22:49 -0500

    first try

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, showDefaultLang } from "./ui";
+import { ui, defaultLang, showDefaultLang, routes } from "./ui";
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
@@ -12,8 +12,55 @@ export function useTranslations(lang: keyof typeof ui) {
   };
 }
 
+export function getRouteFromUrl(url: URL): string | undefined {
+  const pathname = new URL(url).pathname;
+
+  const parts = pathname?.split("/");
+
+  const path = parts.pop() || parts.pop();
+
+  if (path === undefined) {
+    return undefined;
+  }
+
+  const currentLang = getLangFromUrl(url);
+
+  if (defaultLang === currentLang) {
+    const route = Object.values(routes)[0];
+
+    return route[path] !== undefined ? route[path] : undefined;
+  }
+
+  const getKeyByValue = (
+    obj: Record<string, string>,
+    value: string
+  ): string | undefined => {
+    if (obj === undefined) {
+      return undefined;
+    }
+    return Object.keys(obj).find((key) => obj[key] === value);
+  };
+
+  const reversedKey = getKeyByValue(routes[currentLang], path);
+
+  if (reversedKey !== undefined) {
+    return reversedKey;
+  }
+
+  return undefined;
+}
+
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
+    const pathName = path.replaceAll("/", "");
+    const hasTranslation =
+      defaultLang !== l &&
+      routes[l] !== undefined &&
+      routes[l][pathName] !== undefined;
+    const translatedPath = hasTranslation ? "/" + routes[l][pathName] : path;
+
+    return !showDefaultLang && l === defaultLang
+      ? translatedPath
+      : `/${l}${translatedPath}`;
   };
 }
