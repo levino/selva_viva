eedd9d4e4d74c9f4cd5b4d5a285b774156262c51
Alejo <lawebdealejo@gmail.com>
2023-11-09 12:19:35 -0500

    refactors fields

--- src/cms_collection_schemas/file_collections/projects_page/en/index.js
+++ src/cms_collection_schemas/file_collections/projects_page/en/index.js
@@ -1,17 +1,8 @@
+import { projectsPageFields } from "../projects_page_fields";
+
 export const projectsPageSchemaEn = {
   label: "Projects Page En",
   name: "projects_page_en",
   file: "src/content/projectsPage/en/index.md",
-  fields: [
-    {
-      label: "Page Meta Title",
-      name: "page_meta_title",
-      widget: "string",
-    },
-    {
-      label: "title",
-      name: "title",
-      widget: "string",
-    },
-  ],
+  fields: projectsPageFields,
 };
--- src/cms_collection_schemas/file_collections/projects_page/es/index.js
+++ src/cms_collection_schemas/file_collections/projects_page/es/index.js
@@ -1,17 +1,8 @@
+import { projectsPageFields } from "../projects_page_fields";
+
 export const projectsPageSchemaEs = {
   label: "Projects Page Es",
   name: "projects_page_es",
   file: "src/content/projectsPage/es/index.md",
-  fields: [
-    {
-      label: "Page Meta Title",
-      name: "page_meta_title",
-      widget: "string",
-    },
-    {
-      label: "title",
-      name: "title",
-      widget: "string",
-    },
-  ],
+  fields: projectsPageFields,
 };
