e5e23dfd74f2a40ce93d5fbef22871a492118bbf
Alejo <lawebdealejo@gmail.com>
2023-11-09 09:39:16 -0500

    adds card content to cms schema

--- src/cms_collection_schemas/file_collections/tree_nursery.js
+++ src/cms_collection_schemas/file_collections/tree_nursery.js
@@ -9,6 +9,21 @@ export const treeNurseryPageSchema = {
       widget: "string",
     },
     {
+      label: "Page Card",
+      name: "page_card",
+      summary: "{{fields.title}}",
+      collapsed: true,
+      widget: "object",
+      fields: [
+        { labe: "Title", name: "title", widget: "string" },
+        { labe: "Excerpt", name: "excerpt", widget: "markdown" },
+        { labe: "Link", name: "link", widget: "string" },
+        { labe: "Link Text", name: "link_text", widget: "string" },
+        { labe: "Cover Image", name: "cover_image", widget: "image" },
+        { labe: "Cover Image Alt", name: "cover_image_alt", widget: "string" },
+      ],
+    },
+    {
       label: "Hero",
       name: "hero",
       widget: "list",
