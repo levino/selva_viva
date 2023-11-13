acee4871d8c1a81c7386d8755eee0c6f459e3abe
Alejo <lawebdealejo@gmail.com>
2023-11-13 08:26:36 -0500

    fixes import

--- src/cms_collection_schemas/file_collections/pages_schemas.js
+++ src/cms_collection_schemas/file_collections/pages_schemas.js
@@ -3,7 +3,7 @@ import { frontPageSchemaEs } from "./front_page/front_page_es";
 import { frontPageSchemaEn } from "./front_page/front_page_en";
 import { contactPageSchema } from "./contact";
 import { supportPageSchema } from "./support_page";
-import { aboutUsSchemaDe } from "./about_us_page/about_us_de";
+import { aboutUsSchemaDe } from "./about_us_page/de/index";
 import { theFincaSchemaDe } from "./the_finca_page/de/index";
 import { treeNurseryPageSchema } from "./tree_nursery";
 import { projectsPageSchemaDe } from "./projects_page/de";
