0bd0374fbf19a5955e05dc8fed01d0eb9d4c6557
Alejo <lawebdealejo@gmail.com>
2023-11-13 09:39:17 -0500

    adds schemas for es and en

---
+++ src/cms_collection_schemas/file_collections/the_finca_page/en/index.js
@@ -0,0 +1,8 @@
+import { theFincaFields } from "../fields";
+
+export const theFincaSchemaEn = {
+  label: "The Finca Page En",
+  name: "the_finca_page_en",
+  file: "src/content/theFinca/en/index.md",
+  fields: theFincaFields,
+};
---
+++ src/cms_collection_schemas/file_collections/the_finca_page/es/index.js
@@ -0,0 +1,8 @@
+import { theFincaFields } from "../fields";
+
+export const theFincaSchemaEs = {
+  label: "The Finca Page Es",
+  name: "the_finca_page_es",
+  file: "src/content/theFinca/es/index.md",
+  fields: theFincaFields,
+};
