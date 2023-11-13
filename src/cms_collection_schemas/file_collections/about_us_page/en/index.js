0cdd5b34a6a81fcec12dbdcc930e80ef41cac196
Alejo <lawebdealejo@gmail.com>
2023-11-13 08:32:26 -0500

    creates schemas for En and Es

---
+++ src/cms_collection_schemas/file_collections/about_us_page/en/index.js
@@ -0,0 +1,8 @@
+import { aboutUsPageSchemaFields } from "./fields";
+
+export const aboutUsSchemaEn = {
+  label: "About Us En",
+  name: "about_us_en",
+  file: "src/content/aboutUsPage/en/index.md",
+  fields: aboutUsPageSchemaFields,
+};
---
+++ src/cms_collection_schemas/file_collections/about_us_page/es/index.js
@@ -0,0 +1,8 @@
+import { aboutUsPageSchemaFields } from "./fields";
+
+export const aboutUsSchemaEs = {
+  label: "About Us Es",
+  name: "about_us_es",
+  file: "src/content/aboutUsPage/es/index.md",
+  fields: aboutUsPageSchemaFields,
+};
