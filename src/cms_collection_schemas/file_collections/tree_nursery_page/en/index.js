19d99093de6286e4dfbec0b3cfccfc5b41a1a0d8
Alejo <lawebdealejo@gmail.com>
2023-11-13 09:43:00 -0500

    adds schemas for en and es

---
+++ src/cms_collection_schemas/file_collections/tree_nursery_page/en/index.js
@@ -0,0 +1,8 @@
+import { treeNurseryFields } from "../fields";
+
+export const treeNurserySchemaEn = {
+  label: "Tree Nursery Page En",
+  name: "tree_nursery_page_en",
+  file: "src/content/treeNursery/en/index.md",
+  fields: treeNurseryFields,
+};
---
+++ src/cms_collection_schemas/file_collections/tree_nursery_page/es/index.js
@@ -0,0 +1,8 @@
+import { treeNurseryFields } from "../fields";
+
+export const treeNurserySchemaEs = {
+  label: "Tree Nursery Page Es",
+  name: "tree_nursery_page_es",
+  file: "src/content/treeNursery/es/index.md",
+  fields: treeNurseryFields,
+};
