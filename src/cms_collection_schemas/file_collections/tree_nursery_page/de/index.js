110a36e53fb60edc43a5e4cc1564c8693d01be94
Alejo <lawebdealejo@gmail.com>
2023-11-13 09:39:48 -0500

    adds schema to de

---
+++ src/cms_collection_schemas/file_collections/tree_nursery_page/de/index.js
@@ -0,0 +1,8 @@
+import { treeNurseryFields } from "../fields";
+
+export const treeNurserySchemaDe = {
+  label: "Tree Nursery Page De",
+  name: "tree_nursery_page_de",
+  file: "src/content/treeNursery/de/index.md",
+  fields: treeNurseryFields,
+};
