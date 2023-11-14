2a89cd4e19f9925f957399acff73b622bfc37686
Alejo <lawebdealejo@gmail.com>
2023-11-14 08:57:11 -0500

    changes names

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,13 @@ import pages_schemas from "./src/cms_collection_schemas/file_collections/pages_s
 import { postsSchema } from "./src/cms_collection_schemas/folder_collections/posts";
 import tailwind from "@astrojs/tailwind";
 import { teamMembersSchema } from "./src/cms_collection_schemas/folder_collections/teamMembers";
-import collection_schemas from "./src/cms_collection_schemas/folder_collections/collection_schemas";
+import folderCollections from "./src/cms_collection_schemas/folder_collections/folder_collections";
 //this are the collections from the cms
 const {
   plantAndProtectSchemaDe,
   plantAndProtectSchemaEn,
   plantAndProtectSchemaEs,
-} = collection_schemas;
+} = folderCollections;
 
 // https://astro.build/config
 export default defineConfig({
--- src/cms_collection_schemas/folder_collections/folder_collections.js
+++ src/cms_collection_schemas/folder_collections/folder_collections.js
@@ -1,11 +1,11 @@
-import { plantAndProtectSchemaDe } from "./plant_and_protect/de/";
+import { plantAndProtectSchemaDe } from "./plant_and_protect/de";
 import { plantAndProtectSchemaEn } from "./plant_and_protect/en";
 import { plantAndProtectSchemaEs } from "./plant_and_protect/es";
 
-const collectionSchemas = {
+const folderCollections = {
   plantAndProtectSchemaDe,
   plantAndProtectSchemaEn,
   plantAndProtectSchemaEs,
 };
 
-export default collectionSchemas;
+export default folderCollections;
