7451578a29a114631e8570df4e6a20be5ff22881
Alejo <lawebdealejo@gmail.com>
2023-11-14 08:03:39 -0500

    refactors plant and protect

---
+++ src/cms_collection_schemas/folder_collections/plant_and_protect/de/index.js
@@ -0,0 +1,11 @@
+import { plantAndProtectFields } from "../fields";
+export const plantAndProtectSchemaDe = {
+  name: "plant_and_protect_de",
+  label: "Plant and protect De",
+  label_singular: "Modality",
+  folder: "src/content/plantAndProtect/de/",
+  identifier_field: "name",
+  create: true,
+  delete: true,
+  fields: plantAndProtectFields,
+};
---
+++ src/cms_collection_schemas/folder_collections/plant_and_protect/fields.js
@@ -0,0 +1,80 @@
+export const plantAndProtectFields = [
+  { name: "title", widget: "string", label: "Title" },
+  {
+    name: "page_title",
+    widget: "string",
+    label: "Page Title",
+  },
+  {
+    name: "bg_image",
+    widget: "image",
+    label: "Bg Image",
+  },
+  {
+    name: "bg_alt",
+    widget: "string",
+    label: "Bg Alt",
+  },
+  {
+    name: "subtitle",
+    widget: "string",
+    label: "Subtitle",
+  },
+  {
+    name: "excerpt",
+    widget: "text",
+    label: "Excerpt",
+  },
+  { name: "description_title", widget: "string", label: "Description Title" },
+  {
+    name: "description",
+    widget: "markdown",
+    label: "Description",
+  },
+  {
+    name: "what_you_get",
+    label: "What you get",
+    widget: "object",
+    fields: [
+      { name: "title", label: "Title", widget: "string" },
+      {
+        label: "List Items",
+        name: "list_items",
+        widget: "list",
+        summary: "{{fields.name}}",
+        field: { label: "Item", name: "item", widget: "string" },
+      },
+      {
+        label: "Image",
+        name: "image",
+        widget: "image",
+      },
+      {
+        label: "Image Alt",
+        name: "image_alt",
+        widget: "string",
+      },
+    ],
+  },
+  {
+    name: "button_text",
+    widget: "string",
+    label: "Button Text",
+  },
+  {
+    name: "button_link",
+    widget: "string",
+    label: "Button Link",
+  },
+  {
+    name: "back_button_text",
+    widget: "string",
+    label: "Back Button Text",
+  },
+  {
+    name: "cover_image",
+    label: "Cover Image",
+    widget: "image",
+  },
+  { name: "cover_alt", label: "Cover Alt", widget: "string" },
+];
