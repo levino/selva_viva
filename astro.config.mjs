fa2b18af84a50089b0d9ea59f5584a723ed92692
Alejo <lawebdealejo@gmail.com>
2023-11-14 11:11:56 -0500

    removes cms for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import NetlifyCMS from "astro-netlify-cms";
+//import NetlifyCMS from "astro-netlify-cms";
 import { treesSchema } from "./src/cms_collection_schemas/folder_collections/trees";
 import pages_schemas from "./src/cms_collection_schemas/file_collections/pages_schemas";
 import { postsSchema } from "./src/cms_collection_schemas/folder_collections/posts";
@@ -15,34 +15,7 @@ const {
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    NetlifyCMS({
-      config: {
-        // Use Netlify’s “Git Gateway” authentication and target our default branch
-        disableIdentityWidgetInjection: true,
-        backend: {
-          name: "git-gateway",
-          branch: "main",
-        },
-        // Configure where our media assets are stored & served from
-        media_folder: "src/assets/",
-        public_folder: "/src/assets/",
-        // Configure the content collections
-        collections: [
-          pages_schemas,
-          treesSchema,
-          postsSchema,
-          plantAndProtectSchemaDe,
-
-          plantAndProtectSchemaEn,
-          plantAndProtectSchemaEs,
-          teamMembersSchema,
-        ],
-      },
-    }),
-    ,
-    tailwind(),
-  ],
+  integrations: [tailwind()],
   experimental: {
     assets: true,
   },
