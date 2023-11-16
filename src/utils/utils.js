47a2f9b4daf2728a28595659fd2b3d29197a088a
Alejo <lawebdealejo@gmail.com>
2023-11-16 08:47:41 -0500

    adds helper function to choose the path

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,14 @@
+import { getRelativeLocaleUrl } from "astro:i18n";
+import { useTranslationsForLinks } from "../i18n/utils";
+
 export function pickLangPath(lang) {
   return lang === "en" ? `/` : `/${lang}/`;
 }
+
+export const getRoute = (lang, route) => {
+  const r = useTranslationsForLinks(lang);
+  return `${getRelativeLocaleUrl(lang)}${r(route)}`;
+};
 
 // const saveThisLink = `${pickLangPath(lang)}projects/${
 //   project.slug === "tree-nursery"
